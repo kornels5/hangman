8c147ad33465a92bc88758a11423ba9c9dec810e
Kornel Sroka <srokakornel@gmail.com>
2018-06-08 14:59:08 +0200

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const product = {
-    name: 'Lekarze'
+const Hangman = function(word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c']
 }
 
-console.log(product.hasOwnProperty('name'))
+Hangman.prototype.getPuzzle = function() {
+    let puzzle = ''
+
+    this.word.forEach(letter => {
+       if(this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+       } else {
+            puzzle += '*'
+       }
+    })
+
+    return puzzle
+}
+
+const game1 = new Hangman('kat', 2)
+console.log(game1.getPuzzle())
+
