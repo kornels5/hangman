6593f114112d9348619f429d131d6b092f70b4e0
Kornel Sroka <srokakornel@gmail.com>
2018-06-14 13:43:02 +0200

    async request

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-const Hangman = function(word, remainingGuesses) {
-    this.word = word.toLowerCase().split('')
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['c']
-}
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
 
-Hangman.prototype.getPuzzle = function() {
-    let puzzle = ''
+const game1 = new Hangman('Cat smell', 2)
 
-    this.word.forEach(letter => {
-       if(this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter
-       } else {
-            puzzle += '*'
-       }
-    })
+puzzleEl.textContent = game1.puzzle
+guessesEl.textContent = game1.statusMessage
 
-    return puzzle
-}
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode) 
+    game1.makeGuess(guess)
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
 
-const game1 = new Hangman('kat', 2)
-console.log(game1.getPuzzle())
+})
+
+getPuzzleAsync((error, puzzle) => {
+    if(error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
+    }
+})
+
+// const puzzle = getPuzzleSync()
+// console.log(puzzle)
+// console.log('Do something else')
+
+
+//making an HTTP request
+
+// //making http request
+// const countryRequest = new XMLHttpRequest()
+// const countryCode = "PL"
+
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if(e.target.readyState === 4 && e.target.status === 200) {        
+//         const data = JSON.parse(e.target.responseText)
+//         //const country = data.filter((country) => {return country.alpha2Code === countryCode})
+//         const country = data.find((country) => country.alpha2Code === countryCode)
+//         console.log(country)
+        
+//     } else if (e.target.readyState === 4) {
+//         console.log('error');
+//     }
+// })
+
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
+
 
